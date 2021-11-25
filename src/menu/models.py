ffb8f051981818aee7f3aa8380dcd804a36f0eb7
mBuilt <77763183+mBuilt@users.noreply.github.com>
2021-11-24 22:50:41 -0500

    v0.2
    Deprecated BootStrap5 -> replaced by style.css (personally created)
    Flexbox usage
    
    Added proper header & footer
    Layout is slightly different
    Page: Menu is almost complete.

--- src/menu/models.py
+++ src/menu/models.py
@@ -4,14 +4,15 @@ from django_resized import ResizedImageField
 # Create your models here.
 class Food(models.Model):
     name = models.CharField(max_length = 120)
-    price = models.DecimalField(null=True,max_digits=4,decimal_places=2)
-    description = models.TextField(default = '')
     type_choices = [
         ('AP', 'Appetizer'),
         ('MN', 'Main'),
         ('DT', 'Dessert'),
     ]
     type = models.CharField(max_length = 2, choices = type_choices, null = True)
+    price = models.DecimalField(null=True,max_digits=4,decimal_places=2)
+    description = models.TextField(null=True, blank=True)
+
     image = ResizedImageField(size=[400,400], quality=100, upload_to="food")
 
     def __str__(self):
