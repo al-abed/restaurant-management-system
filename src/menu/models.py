72a7757a640629bb91e876fb87ef11352156c178
mBuilt <77763183+mBuilt@users.noreply.github.com>
2021-11-29 11:26:37 -0500

    changes to formatting
    food model attribute type renamed to courses
    
    removed conditional if statements within menu.html food descriptions.

--- src/menu/models.py
+++ src/menu/models.py
@@ -4,12 +4,12 @@ from django_resized import ResizedImageField
 # Create your models here.
 class Food(models.Model):
     name = models.CharField(max_length = 120)
-    type_choices = [
+    course_choices = [
         ('AP', 'Appetizer'),
         ('MN', 'Main'),
         ('DT', 'Dessert'),
     ]
-    type = models.CharField(max_length = 2, choices = type_choices, null = True)
+    course = models.CharField(max_length = 2, choices = course_choices, null = True)
     price = models.DecimalField(null=True,max_digits=4,decimal_places=2)
     description = models.TextField(null=True, blank=True)
     image = ResizedImageField(size=[400,400], quality=100, upload_to="food", blank=True, default="default.jpg")
